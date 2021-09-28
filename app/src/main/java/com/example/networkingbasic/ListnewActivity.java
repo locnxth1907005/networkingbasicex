3c123c7dff4b75e11335c99ea440f7ffb4ac4859
locnxth1907005 <locnxth1907005@fpt.edu.vn>
2021-09-28 19:57:29 +0700

    1

---
+++ app/src/main/java/com/example/networkingbasic/ListnewActivity.java
@@ -0,0 +1,13 @@
+package com.example.networkingbasic;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListnewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listnew);
+    }
+}
---
+++ app/src/main/java/com/example/networkingbasic/adapter/NewsAdapter.java
@@ -0,0 +1,4 @@
+package com.example.networkingbasic.adapter;
+
+public class NewsAdapter {
+}
